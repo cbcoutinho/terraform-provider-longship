06e9de299384e34b1d32c8925ff7794a519d1eec
Chris Coutinho <chris.coutinho@milence.com>
2023-07-08 01:52:25 +0200

    Move example to separate directory, simplify docs

--- examples/main.tf
+++ examples/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_providers {
+    longship = {
+      version = "0.1.2"
+      source  = "milence.com/data-platform/longship"
+    }
+  }
+}
+
+provider "longship" {}
+
 resource "longship_webhook" "example" {
   name        = "test"
   ou_code     = "0000"
