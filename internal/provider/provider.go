e1bd2ac17147804d2a17e06b46cd161f1046086d
Chris Coutinho <chris.coutinho@milence.com>
2023-07-09 02:23:45 +0200

    feat(chargepoints): Add Chargepoints data source

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -204,6 +204,7 @@ func (p *longshipProvider) Configure(ctx context.Context, req provider.Configure
 // DataSources defines the data sources implemented in the provider.
 func (p *longshipProvider) DataSources(_ context.Context) []func() datasource.DataSource {
 	return []func() datasource.DataSource{
+		NewChargepointsDataSource,
 		NewWebhooksDataSource,
 	}
 }
